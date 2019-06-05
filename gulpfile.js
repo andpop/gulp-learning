01bbeb1dbbf7b5887629e8148cc00374eb93a0a8
Andrey Popov <andpop@mail.ru>
2019-06-05 06:26:11 +0300

    Video 6

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 const { src, dest, task, series } = require("gulp");
 var rm = require('gulp-rm');
 
-task( 'clean', () => {
-    return src( 'dist/**/*', { read: false })
-      .pipe( rm() )
-  })
+task('clean', () => {
+  return src('dist/**/*', { read: false })
+    .pipe(rm())
+})
 
-task( 'copy', () => {
-    return src('src/styles/*.scss').pipe(dest('dist'));
-  })
+task('copy', series('clean', () => {
+  return src('src/styles/*.scss').pipe(dest('dist'));
+}))
